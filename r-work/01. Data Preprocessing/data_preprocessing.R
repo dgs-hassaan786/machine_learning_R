d5bc73e1781c6dbf2ca67e102a8ac4ad0a1d18c4
hassaan <hassaankhan786@gmail.com>
2019-01-23 17:10:01 +0500

    Progress 1: working on data pre-processing including dataset import, cleaning and other tactics
    Signed-off-by: hassaan <hassaankhan786@gmail.com>

---
+++ r-work/01. Data Preprocessing/data_preprocessing.R
@@ -0,0 +1,6 @@
+#Data Preprocessing
+
+#importing the dataset
+
+working_dir <- 'F:\\workspaces\\machine_learning\\Machine_Learning_AZ_Template_Folder\\Part 1 - Data Preprocessing\\Section 2 -------------------- Part 1 - Data Preprocessing --------------------\\Data.csv'
+dataset = read.csv(working_dir)
